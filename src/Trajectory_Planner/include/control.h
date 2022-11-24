13767a1cfa5c7279c42d16545ef81ec6ab4a714a
MEZHANGYUE <2039848588@qq.com>
2022-11-24 15:03:29 +0800

    closed loop

--- src/Trajectory_Planner/include/control.h
+++ src/Trajectory_Planner/include/control.h
@@ -7,6 +7,13 @@
 #include <mavros_msgs/State.h>
 #include <geometry_msgs/Pose.h>
 #include <nav_msgs/Path.h>
+#include "geometry_msgs/TwistStamped.h"
+struct XYZ
+{
+    float x;
+    float y;
+    float z;
+};
 class control
 {
     private:
@@ -14,11 +21,15 @@ class control
         ros::ServiceClient arming_client;     //解锁服务
         ros::ServiceClient set_mode_client;    //模式设置   offboard
         ros::Publisher local_pos_pub;  ///发布无人机位置控制
-        
+        ros::Publisher local_vel_pub;  ///发布无人机速度控制
         ros::Subscriber state_sub;
         ros::Subscriber pose_sub;
         ros::Subscriber path_sub;      // 订阅生成的优化轨迹
         ros::Publisher realpath_pub;   // 发布实际生成的轨迹
+        geometry_msgs::TwistStamped vel;
+        XYZ curr_error;
+        XYZ last_error;
+        float kp , ki , kd;
     public:
         control()                               //构造函数；
         {
@@ -28,8 +39,10 @@ class control
         }     
         mavros_msgs::SetMode offb_set_mode;
         mavros_msgs::CommandBool arm_cmd;
+        int mode_set (void);
         void px4_init(void);
         void planpath_sub(void);   //订阅轨迹
+        geometry_msgs::TwistStamped PID_vel(geometry_msgs::PoseStamped cuur_pose , geometry_msgs::PoseStamped targ_pose);  //计算PID
         void px4_follow(void);
         void px4_control(void);
         //两个回调函数
--- src/Trajectory_Planner/src/control.cpp
+++ src/Trajectory_Planner/src/control.cpp
@@ -23,9 +23,31 @@ void pose_cb(const geometry_msgs::PoseStamped::ConstPtr &pt)
 /*
 **       初始化px4，通信连接、解锁、模式设置
 */
+int control::mode_set (void)
+{
+    if( current_state.mode != "OFFBOARD")
+            if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent)
+            {
+                ROS_INFO("Offboard enabled");//打开模式后打印信息
+            }
+        
+    if( !current_state.armed)
+        if( arming_client.call(arm_cmd) && arm_cmd.response.success)
+        {
+            ROS_INFO("Vehicle armed");//解锁后打印信息
+        }
+    
+    if (current_state.armed && current_state.mode == "OFFBOARD")   return 1;
+    else return 0;
+    
+}
+
 void control::px4_init(void)
 {
     ros::Rate rate(20.0);  //设置发布速率
+    local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 10);
+    local_vel_pub = nh.advertise<geometry_msgs::TwistStamped> ("/mavros/setpoint_velocity/cmd_vel", 10); 
+    realpath_pub = nh.advertise<nav_msgs::Path>("/real_trajectory", 10);
     state_sub = nh.subscribe<mavros_msgs::State>("/mavros/state", 1, state_cb);     //订阅飞机状态
     pose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 10, pose_cb);     //订阅飞机位置pose
     arming_client = nh.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");     //解锁服务
@@ -35,24 +57,40 @@ void control::px4_init(void)
         ros::spinOnce();
         rate.sleep();
     }
-    
     offb_set_mode.request.custom_mode = "OFFBOARD";    // 模式设置
-    
     arm_cmd.request.value = true;                           // 解锁
 }
 
 /*
-**订阅轨迹话题
+********订阅轨迹话题
 */
 void control::planpath_sub(void)
 {
     path_sub = nh.subscribe<nav_msgs::Path>("/tra_generation", 10,trajectory_cb);     //订阅轨迹消息
 } 
 
+geometry_msgs::TwistStamped control::PID_vel(geometry_msgs::PoseStamped cuur_pose , geometry_msgs::PoseStamped targ_pose)
+{
+    geometry_msgs::TwistStamped _vel;
+    _vel.header.frame_id = "/map";
+    _vel.header.stamp = ros::Time::now();
+
+    curr_error.x = cuur_pose.pose.position.x - targ_pose.pose.position.x;
+    curr_error.y = cuur_pose.pose.position.y - targ_pose.pose.position.y;
+    curr_error.z = cuur_pose.pose.position.z - targ_pose.pose.position.z;
+
+    _vel.twist.linear.x = curr_error.x * kp;
+    _vel.twist.linear.y = curr_error.y * kp;
+    _vel.twist.linear.z = curr_error.z * kp;
+    // std::cout << "_vel:"  << _vel <<std::endl;
+    return _vel;
+}
+
+/*
+*********************无人机路径跟随
+*/
 void control::px4_follow(void)
 {
-    local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 10);
-    realpath_pub = nh.advertise<nav_msgs::Path>("/real_trajectory", 10);
     geometry_msgs::PoseStamped pose;
     nav_msgs::Path real_path;
     pose.header.frame_id = "/map";
@@ -62,24 +100,23 @@ void control::px4_follow(void)
     ros::Rate rate(60); 
     for(int i=0; i < plan_tra.poses.size(); i++)
     {
-        pose.pose.position.x = plan_tra.poses[i].pose.position.x;
-        pose.pose.position.y = plan_tra.poses[i].pose.position.y;
-        pose.pose.position.z = plan_tra.poses[i].pose.position.z;
-        local_pos_pub.publish(pose);
-
+        
         pose.pose.position.x = px4_pose.pose.position.x;
         pose.pose.position.y = px4_pose.pose.position.y;
         pose.pose.position.z = px4_pose.pose.position.z;
         // std::cout << "pose:  " << pose << std::endl;
         real_path.poses.push_back(pose);
         realpath_pub.publish(real_path);
+
+        pose.pose.position.x = plan_tra.poses[i].pose.position.x;
+        pose.pose.position.y = plan_tra.poses[i].pose.position.y;
+        pose.pose.position.z = plan_tra.poses[i].pose.position.z;
+
+        vel = PID_vel(px4_pose , plan_tra.poses[i]);
+        local_vel_pub.publish(vel);
+        // local_pos_pub.publish(pose);
         ros::spinOnce();
         rate.sleep();
-    }
-    while(1)
-    {
-        ros::spinOnce();
-        local_pos_pub.publish(pose);
     }
 }
  /*
@@ -87,30 +124,15 @@ void control::px4_follow(void)
  */
 void control::px4_control(void)
 {
+    kp = -2;
     px4_init();
     planpath_sub();
     while(1)
     {
-        if( current_state.mode != "OFFBOARD")
-                if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent)
-                {
-                    // ROS_INFO("Offboard enabled");//打开模式后打印信息
-                }
-        else 
-        {
-            if( !current_state.armed)
-                if( arming_client.call(arm_cmd) && arm_cmd.response.success)
-                {
-                    ROS_INFO("Vehicle armed");//解锁后打印信息
-                }
-        }
-        // if(current_state.mode == "OFFBOARD" && current_state.armed) break;
-        if( current_state.armed) 
-            px4_follow();
-        ros::spinOnce();
+        mode_set();
+        px4_follow();
         ros::spinOnce();
     }
-    
     
 }
 
@@ -121,12 +143,6 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "offb_node"); 
     control control;
-while (1)
-{
-    // std::cout <<"plan_tra:" << control::plan_tra << std::endl;
     
-}
-
- 
     return 0;
 }
